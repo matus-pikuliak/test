0bf7bb87733bf637e7534057dfe954965efa3078
Matúš Pikuliak <matus.pikuliak@gmail.com>
2024-11-16 16:09:31 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ def read_requirements(filename):
 setup(
     name="test",
     version="0.1",
+    packages=find_namespace_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=read_requirements("requirements.txt"),
-    packages=find_namespace_packages(
-        exclude=["runs"],
-    ),
 )
